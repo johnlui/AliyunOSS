89704ad3e9dccfa081b6d5344153e1bbd54c933d
JohnLui <wenhanlv@gmail.com>
2015-01-09 22:25:05 +0800

    update OSSExample.php

--- OSSExample.php
+++ OSSExample.php
@@ -1,1 +1,48 @@
-OSSExample.php
+<?php
+
+namespace App\Services;
+
+use JohnLui\AliyunOSS\AliyunOSS;
+
+use Config;
+
+class OSS {
+
+  private $ossClient;
+
+  public function __construct()
+  {
+    $this->ossClient = AliyunOSS::boot(
+      Config::get('app.ossServerInternal'),
+      Config::get('app.AccessKeyId'),
+      Config::get('app.AccessKeySecret')
+    );
+  }
+
+  public static function upload($ossKey, $filePath)
+  {
+    $oss = new OSS();
+    $oss->ossClient->setBucket('yishuodian-test');
+    $oss->ossClient->uploadFile($ossKey, $filePath);
+  }
+
+  public static function getUrl($ossKey)
+  {
+    $oss = new OSS();
+    $oss->ossClient->setBucket('yishuodian-test');
+    return $oss->ossClient->getUrl($ossKey, new \DateTime("+1 day"));
+  }
+
+  public static function createBucket($bucketName)
+  {
+    $oss = new OSS();
+    return $oss->ossClient->createBucket($bucketName);
+  }
+
+  public static function getAllObjectKey($bucketName)
+  {
+    $oss = new OSS();
+    return $oss->ossClient->getAllObjectKey($bucketName);
+  }
+
+}
